eb7ca76fb546e33d11e3575ce2f412b0c315fa9e
jan <rodriguez.janrichard@gmail.com>
2019-08-28 15:21:02 -0700

    Code cleanup

--- Trees/sortedArrayToBST.py
+++ Trees/sortedArrayToBST.py
@@ -11,17 +11,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: TreeNode
         """
-
-        def recursiveConvert(self, left_index, right_index):
+        def recursiveConvert(left_index, right_index):
             if left_index > right_index:
                 return None
             mid = (left_index + right_index) / 2
             temp = TreeNode(nums[mid])
-            temp.left = recursiveConvert(self, left_index, mid - 1)
-            temp.right = recursiveConvert(self, mid + 1, right_index)
+            #Start adding children recursively
+            temp.left = recursiveConvert(mid - 1)
+            temp.right = recursiveConvert(right_index)
             return temp
 
-        return recursiveConvert(self, 0, len(nums) - 1)
+        return recursiveConvert(0, len(nums) - 1)
 
 
 
